13b8e4bc7c9a2edcccc6b7116ec1bad3ea7698b9
juliet-wambua <julienea289@gmail.com>
2019-08-29 10:46:44 +0300

    journalActivity.java

--- app/src/main/java/com/nea/myjournal/JournalActivity.java
+++ app/src/main/java/com/nea/myjournal/JournalActivity.java
@@ -2,13 +2,46 @@ package com.nea.myjournal;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class JournalActivity extends AppCompatActivity {
+    @BindView(R.id.locationTextView) TextView mLocationTextView;
+    @BindView(R.id.listView) ListView mListView;
+
+    private String[] journals = new String[] {"A trip to Mwanza","First day at Moringa School", "My Love life"};
+    private String[]  About = new String[] {"Tanzania", "Moringa", "Queen of Hearts"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_journal);
+        ButterKnife.bind(this);
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, journals);
+        mListView.setAdapter(adapter);
+
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                String journal = ((TextView)view).getText().toString();
+                Toast.makeText(JournalActivity.this, journal, Toast.LENGTH_LONG).show();
+                Log.v("JournalActivity", "In the onItemClickListener!");
+            }
+        });
+
+        Intent intent = getIntent();
+        String location = intent.getStringExtra("location");
+        mLocationTextView.setText("Here is a list of all the journals i have created: " + location);
     }
 }
