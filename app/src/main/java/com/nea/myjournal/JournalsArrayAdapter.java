bda086beb777e40e32640e67ba0a9761220102ea
juliet-wambua <julienea289@gmail.com>
2019-08-29 10:55:47 +0300

    implemented the respective methods to the ArrayAdapter

--- app/src/main/java/com/nea/myjournal/JournalsArrayAdapter.java
+++ app/src/main/java/com/nea/myjournal/JournalsArrayAdapter.java
@@ -1,4 +1,29 @@
 package com.nea.myjournal;
 
-public class JournalsArrayAdapter {
+import android.widget.ArrayAdapter;
+import android.content.Context;
+
+public class JournalsArrayAdapter extends ArrayAdapter{
+    private Context mContext;
+    private String[] mJournals;
+    private String[] mAbout;
+
+    public JournalsArrayAdapter(Context mContext, int resource, String[] mJournals, String[] mAbout) {
+        super(mContext, resource);
+        this.mContext = mContext;
+        this. mJournals = mJournals;
+        this.mAbout = mAbout;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        String journal = mJournals[position];
+        String about = mAbout[position];
+        return String.format("%s \nServes great: %s", journal, about);
+    }
+
+    @Override
+    public int getCount() {
+        return mJournals.length;
+    }
 }
