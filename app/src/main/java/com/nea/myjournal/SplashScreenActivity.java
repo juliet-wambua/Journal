f15ced540fc3f105a8e4afebf244d4265cabd2ae
juliet-wambua <julienea289@gmail.com>
2019-08-29 10:40:55 +0300

    added functionality to the slashscreenActivity.java

--- app/src/main/java/com/nea/myjournal/SplashScreenActivity.java
+++ app/src/main/java/com/nea/myjournal/SplashScreenActivity.java
@@ -1,14 +1,37 @@
 package com.nea.myjournal;
-
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.nea.myjournal.R;
 
 public class SplashScreenActivity extends AppCompatActivity {
+
+    private int SLEEP_TIMER = 3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_splash_screen);
+        getSupportActionBar().hide();
+
+
+        LogoLauncher logoLauncher = new LogoLauncher();
+        logoLauncher.start();
+    }
+    private class LogoLauncher extends Thread{
+        public void run() {
+            try{
+                sleep(1500 * SLEEP_TIMER);
+            } catch (InterruptedException e) {
+                e.printStackTrace(); }
+            Intent intent = new Intent(SplashScreenActivity.this, MainActivity.class);
+            startActivity(intent);
+            SplashScreenActivity.this.finish();
+        }
     }
 }
